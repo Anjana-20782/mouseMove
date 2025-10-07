78e898a7f180f519c382ea6bb1bc15b85f8de107
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-07 13:04:56 +0530

    new contents added the mouse move

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let cursor=document.getElementById("cursor")
 document.addEventListener("mousemove",function(e){
 
-    cursor.style.left=e.clientX -30+ "px"
-    cursor.style.top=e.clientY -30+ "px"
+    cursor.style.left=e.clientX -20+ "px"
+    cursor.style.top=e.clientY -20+ "px"
 });
+document.body.style.cursor="url('/icons8-cursor.svg')"
